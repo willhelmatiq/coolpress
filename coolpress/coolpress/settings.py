54aca08ef4bbe68afa06ebf9759ecf390e824750
willhelmatiq <rot3ad@gmail.com>
2022-11-13 19:33:15 +0300

    Homework3 + fix migration scripts

--- coolpress/coolpress/settings.py
+++ coolpress/coolpress/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'coolpress.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbProdData.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
